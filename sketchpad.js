191ed2bbcb8b78c0515b59f137d7c9d51c437d6f
Amy <138383945+amycatgirl@users.noreply.github.com>
2024-03-15 19:17:19 -0400

    Update sketchpad.js

--- sketchpad.js
+++ sketchpad.js
@@ -1,19 +1,50 @@
 const canvas = document.getElementById('drawandchat');
 const colourInput = document.getElementById('colour');
 const sizeInput = document.getElementById('size');
-const captionInput = document.getElementById('caption');
-
+const captionInput = document.getElementById('caption')
 const ctx = canvas.getContext('2d');
 
 var skip1, skip2, cp1x, cp2x, cp1y, cp2y;
 
+var currentColour = "#000"
+var currentSize = 5
+
 ctx.fillStyle = "#fff";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-let coords = {
-	x: 0,
-	y: 0
-};
+colourInput.addEventListener('change', (ev) => {
+	currentColour = ev.target.value
+})
+
+sizeInput.addEventListener('change', (ev) => {
+	currentSize = ev.target.value
+})
+
+for (const button of document.querySelectorAll(".picker > button")) {
+	button.addEventListener('click', () => {
+		currentColour = button.id
+		colourInput.value = currentColour
+	})
+}
+
+for (const button of document.querySelectorAll(".pentools > button")) {
+	if (button.id === "pen") {
+		button.addEventListener('click', () => {
+			currentColour = "#000000"
+			colourInput.value = currentColour
+		})
+	} else if (button.id === "eraser") {
+		button.addEventListener('click', () => {
+			currentColour = "#ffffff"
+			colourInput.value = currentColour
+		})
+	} else {
+		button.addEventListener('click', () => {
+			currentSize = button.id
+			sizeInput.value = currentSize
+		})
+	}
+}
 
 var pressed = false;
 
@@ -28,8 +59,8 @@ function getMousePos(ev) {
 function draw2(x, y) {
 	ctx.lineCap = 'round';
 	ctx.lineJoin = 'round';
-	ctx.lineWidth = sizeInput.value;
-	ctx.strokeStyle = colourInput.value;
+	ctx.lineWidth = currentSize;
+	ctx.strokeStyle = currentColour;
 	
 	if (!pressed) {
 		ctx.beginPath();
@@ -50,7 +81,9 @@ function draw2(x, y) {
 			skip1 = false;
 			skip2 = false;
 		} else {
+			//ctx.lineTo(x,y);
 			ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+			//ctx.quadraticCurveTo(cp1x, cp1y, x, y);
 			skip1 = true;
 			skip2 = false;
 		}
@@ -60,13 +93,14 @@ function draw2(x, y) {
 
 function draw(ev) {
 	const pos = getMousePos(ev);
-	draw2(pos.x, pos.y);
+	draw2(pos.x, pos.y)
 }
 
 function clearAll() {
-	canvas.width = canvas.width;
+	canvas.width = canvas.width
 	ctx.fillStyle = "#fff";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	// apparently it's that simple
 }
 
 function convertToImage() {
@@ -83,10 +117,20 @@ async function startUpload() {
         caption.value = "";
 }
 
+
 canvas.addEventListener('mousedown', (ev) => {
-	canvas.addEventListener('mousemove', draw, false);
-	draw(ev);
-}, false);
+	canvas.addEventListener('mousemove', draw, false)
+	const mousePos = getMousePos(ev)
+	ctx.fillStyle = currentColour;
+	ctx.fillRect(mousePos.x, mousePos.y, currentSize, currentSize)
+	ctx.stroke()
+}, false)
+
+canvas.onmouseleave = () => {
+	canvas.removeEventListener('mousemove', draw, false);
+	pressed = false; 
+	ctx.save();
+}
 
 canvas.addEventListener('mouseup', function() {
 	canvas.removeEventListener('mousemove', draw, false);
