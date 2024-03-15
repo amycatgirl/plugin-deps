dc7867799bba678e6abc65e994fcac9c3f04e0c4
Amy <138383945+amycatgirl@users.noreply.github.com>
2024-03-15 19:15:14 -0400

    Update sketchpadTemplate.js

--- sketchpadTemplate.js
+++ sketchpadTemplate.js
@@ -1,68 +1,104 @@
   window.sketchpad._css = `
-      .sketchpad.container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-        width: 100%;
+.sketchpad.container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
 
-        gap: 1rem;
+  gap: 1rem;
 
-        background-color: var(--primary-header);
-        color: var(--foreground);
-      }
+  background-color: #0a0c1d;
+  color: #fff;
+}
 
-      .sketchpad.container > canvas {
-        border-radius: 10px;
-      }
+.sketchpad header {
+  width: 100%;
+}
 
-      .sketchpad .controls, .sketchpad .picker, .sketchpad .size, .sketchpad .options {
-        display: flex;
-        flex-direction: row;
+.sketchpad header > input {
+  width: 100%;
+  padding: 0.5rem;
+  font-size: 1.5rem;
+  border: none;
+  background: transparent;
+  color: #fff;
+}
 
-        align-items: center;
-      }
+.sketchpad header > input::placeholder {
+  font-style: italic;
+}
 
-      .sketchpad > .picker, .size, .options {
-        gap: 1rem;
-      }
+.sketchpad .tooling {
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+}
 
-      .sketchpad .size {
-        margin-left: 1rem;
-	accent-color: var(--accent);
-      }
+.sketchpad .tooling > canvas {
+  flex-grow: 0;
+  flex-shrink: 0;
+}
 
-      .sketchpad .options > button {
-        border: unset;
-        background-color: var(--primary-background);
-        padding: 10px;
-        border-radius: 5px;
-        color: var(--foreground);
-      }
+.sketchpad .tooling > div {
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  gap: 1rem;
+  max-width: 5rem;
+}
+
+.sketchpad .options {
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 3rem;
+}
+
+.sketchpad .info {
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  gap: .5rem;
+}
     `;
     
     window.sketchpad._html = `
-      <div class="sketchpad container">
+<div class="sketchpad container">
 		<header>
-			<h1>Sketchpad</h1>
+			<input type="text" maxlength="2000" id="caption" placeholder="Name your artwork...">
 		</header>
-		<div class="controls">
+		<div class="tooling">
 			<div class="picker">
-				<label for="colour">Colour</label>
+				<button id="#ff0000">Red</button>
+				<button id="#0000ff">Blue</button>
+				<button id="#00ff00"">Green</button>
+				<button id="#FFFF00">Yellow</button>
+				<button id="#FFA500">Orange</button>
+				<button id="#800080">Purple</button>
+				<button id="#FFC0CB">Pink</button>
 				<input type="color" value="#000" id="colour">
 			</div>
-			<div class="size">
-				<label for="size">Size</label>
-				<input type="range" value=4 min=1 max=50 id="size">
+			<canvas id="drawandchat" width=400 height=400></canvas>
+			<div class="pentools">
+				<button id="pen">Pen</button>
+				<button id="eraser">Eraser</button>
+				<button id="10">10px</button>
+				<button id="20">20px</button>
+				<button id="45">45px</button>
+				<button id="65">65px</button>
+				<button id="100">100px</button>
+				<input type="number" placeholder="Custom Size..." value=5 min=1 id="size">
 			</div>
 		</div>
-  		<input type="text" id="caption" placeholder="Add a caption!" value="" maxlength=2000></input>
 		<div class="options">
-			<button id="toimage" onclick="startUpload()">Share</button>
-			<button id="deleteAll" onclick="clearAll()">Clear</button>
-			<button id="close" onclick="window.sketchpad.toggleSketchpad()">Close</button>
+			<button id="toimage" onclick="convertToImage()">Download</button>
+			<button id="deleteAll" onclick="clearAll();">Clear</button>
 		</div>
-      </div>
+		<div class="info">
+			<span>sketchvolt</span> - <span>1.0.0</span>  - <a href="https://github.com/amycatgirl/plugin-deps" target="_blank">Source Code</a>
+		</div>
       <style>${window.sketchpad._css}</style>
     `;
